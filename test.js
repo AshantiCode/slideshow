c5e9b6585b93fb5f9f0abe54499a82655289e4a9
Ashanti <ashanti.haring@gmail.com>
2018-08-09 16:15:06 +0200

    initial commit

---
+++ test.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded',getWeather)
+
+document.addEventListener('DOMContentLoaded',getStatus)
+
+// document.addEventListener('DOMContentLoaded',getStatus)
+
+document.addEventListener('DOMContentLoaded',getWeather)
+document.addEventListener('DOMContentLoaded',getWeather)
+
+const weather = new UIEvent()
+
+document.addEventListener('DOMContentLoaded',getStatus)
